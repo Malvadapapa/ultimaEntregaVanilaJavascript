5409612a1e0509d6b66506e4cde8f80f17cfa771
MalvadaPapa <cristian.v62@gmail.com>
2022-10-29 05:30:41 -0300

    primer commit

---
+++ variables&data.js
@@ -0,0 +1,60 @@
+const diario =
+    [{
+        id: 1,
+        categoria: "CIUDADANOS",
+        titular: "Cerro Champaquí: rescatan en helicóptero a una mujer con ACV y a 12 estudiantes con vómitos",
+        img: [],
+        txt: `El grupo especial de la Policía Duar recibió en la medianoche de este martes el pedido de rescate de dos contingentes que se encuentran en la zona del Cerro Champaquí en Córdoba. Algunas de esas personas fueron descendidas en helicópteros este miércoles para su revisión médica más rápida y adecuada.
+
+        El primero de ellos está compuesto por 54 personas, en su mayoría jóvenes y menores de edad, que forman parte de una excursión de colegios de Rosario.
+        
+        De acuerdo con lo informado por el jefe del grupo especial, Sergio Cravero, 12 de los chicos presentaron distintas descomposturas, con síntomas de vómitos y diarreas. “Están en el albergue respectivo, todos a resguardo y siendo asistidos por el persona sanitario disponible”, agregó el funcionario.
+        
+        Esos 12, con cuadros de gastroenteritis, fueron descendidos al mediodía de este miércoles en un helicóptero provincial hasta el hospital de Santa Rosa de Calamuchita Ningún caso es grave, se anticipó desde el hospital a este diario. Fueron atendidos en la guardia del centro del salud.
+        En tanto, la situación más complicada la protagoniza una mujer de otro contingente, de 21 personas mayores de edad y proveniente de Mar del Plata. Una mujer de 60 años tiene un “ACV en curso”, según confirmó Cravero.
+
+        “Fue estabilizada por el personal sanitario en la posta del cerro y es monitoreada de manera permanente”, agregó, antes de que también fuera bajada en otro helicóptero, que la trasladó al hospital San Roque, de la ciudad de Córdoba, donde ya es monitoreada su evolución.
+
+        Cravero señaló que un grupo de cinco efectivos inició el ascenso al cerro en la medianoche, tras el primer llamado, pero que por las malas condiciones del clima, con muy fuertes vientos, esa tarea se complicó. Tampoco fue posible llegar por el camino al cerro Los Linderos porque dos arroyos estaban crecidos por las lluvias.
+        EL RESCATE SE REALIZA EN HELICÓPTERO PASADAS LAS 10
+        Pasadas las 10, el grupo especial Duar con el auxilio de los integrantes de la Secretaría de Gestión del Riesgo de la Provincia, procedieron a iniciar la evacuación de las personas más afectadas de ambos contingentes.
+
+        El resto bajaría de modo normal, por su cuenta, este miércoles.
+        `,
+        autor: "Redaccion del diario",
+        fecha: "Miércoles, 26 de octubre de 2022 14:35 hs"
+    },
+    {
+        id: 2,
+        categoria: "SUCESOS",
+        titular: "Morteros: detienen a otro hombre por distribuir pornografía infantil",
+        img: [],
+        txt: `Un nuevo caso de distribución de pornografía infantil se detectó en la ciudad de Morteros, siendo detenido un hombre de 25 años por orden de la Fiscalía de Instrucción a cargo de Juan Fernando Ávila Echenique.
+        En las últimas horas se realizó en esta localidad del departamento San Justo un allanamiento en el marco de una investigación que tuvo su origen en un reporte de la National Center for Missing and Exploited Children (NCMEC).
+
+        Según informó la fiscalía, personal de la sección de Cibercrimen de la DIO, a cargo de Nestor Caillet Bois, logró individualizar al autor del delito, quien ya se encuentra a disposición de la Justicia alojado en la comisaría de Morteros.
+
+        El equipo de la DIO, junto a personal policial y brigada de investigaciones de Morteros, diligenciaron el allanamiento en el domicilio del sospechado, logrando acreditar los extremos denunciados y secuestrar material probatorio relevante para la investigación donde se lo investiga por distribuir imágenes pornográficas en las que se exhiben menores de 18 años de edad.
+
+        MÁS CASOS DE PORNOGRAFÍA INFANTIL EN MORTEROS
+        En agosto pasado, también en Morteros, un joven de 18 años fue detenido por tenencia y distribución de imágenes pornográficas de este mismo calibre, donde existen menores de edad de por medio.
+
+        La investigación también fue originada con motivo de un reporte de la NCMEC y llevada a cabo por personal de la sección de Cibercrimen de la DIO.
+
+        Por otro lado, el fiscal Ávila Echenique había requerido en julio de este año la elevación a juicio de otra causa también por este delito y además por tenencia ilegal de armas de fuego de uso civil.
+
+        El imputado, identificado como Nelson Fernando Benedetto, es de la localidad de Balnearia, tiene 44 años y actualmente se encuentra con prisión preventiva.
+        `,
+        autor: "Redaccion del diario",
+        fecha: "Miércoles, 26 de octubre de 2022 15:43 hs"
+    },
+    {
+        id: 3,
+        categoria: "",
+        titular: "",
+        img: [],
+        txt: `
+        `,
+        autor: "",
+        fecha: ""
+    }]
