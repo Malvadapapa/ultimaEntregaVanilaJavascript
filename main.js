b2ede8b3fa70ca6d18cf331ee976e1100b5b176c
MalvadaPapa <cristian.v62@gmail.com>
2022-11-23 11:27:40 -0300

    introduccion de cambios profundos

--- main.js
+++ main.js
@@ -11,8 +11,16 @@ const burgerContainer = document.getElementById("burgerBlurSection")
 /*MENU HAMBURGUESA*/
 const hamMenu = document.getElementById("burgerMenu");
 
+const logginForm = document.getElementById("logginForm")
+const logginUsername =document.getElementById("sesionUsername")
+const logginPassword = document.getElementById("sesionPassword")
+const sesionSmall = document.getElementById("sesionSmall")
+const passwordSmall = document.getElementById("passwordSmall")
 
 
+const CloseLoggInSection = document.getElementById("initSesion")
+const bluredLogginSection = document.getElementById("blurLogginSection")
+const logginBtnToggle = document.getElementById("session")
  //TOGGLE DE MENU HAMBURGUESA//
 
 
@@ -31,11 +39,83 @@ const closeOnClick = (e) => {
 
   };
 
+  const showLogginError = (input, message) =>{
+    const formField = input;
+      formField.classList.add("error")
+    const errorField = formField.parentElement.querySelector("small")
+    errorField.textContent = message;
+}
+
+  const checkLogginUsername  = () =>{
+    const user = logginUsername.value.trim()
+let valid = false;
+ 
+if(user === "" ){
+    showLogginError(logginUsername, "¡Este campo está vacio!" )
+} else{
+    logginUsername.classList.remove("error")
+    sesionSmall.textContent = "";
+    valid = true;
+}
+return valid;
+};                 
+
+const checkLogginRegisterPasword = () =>{
+    let valid = false;
+const passwordLoggin = logginPassword.value.trim();
+if (passwordLoggin === "" ) {
+    showLogginError(logginPassword, "¡Este campo está vacio!");
+} else{
+    logginPassword.classList.remove("error")
+    passwordSmall.textContent = "";
+    valid = true;
+}
+return valid
+};
+
+
+const toogleLogginSection = ()=> {
+    bluredLogginSection.classList.toggle("bluredLoggin");
+ 
+}
+
+const closeOnClickLogginSection = (e) => {
+    if (e.target.classList.contains("iniciarSesionSection")) {
+        bluredLogginSection.classList.toggle("bluredLoggin");
+ 
+}else{
+    return;
+}}
+
 
 
   const init = () => {
     hamMenu.addEventListener("click", toogleBurger);
    burgerContainer.addEventListener("click", closeOnClick);
+   
+   logginBtnToggle.addEventListener("click", toogleLogginSection );
+   CloseLoggInSection.addEventListener("click", toogleLogginSection );
+   bluredLogginSection.addEventListener("click", closeOnClickLogginSection );
+   logginForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    
+        let isLogginUsernameValid = checkLogginUsername ()
+ 
+        let isLogginPaswordValid = checkLogginRegisterPasword ()
+        
+     const isLogginalid =  isLogginUsernameValid &&isLogginPaswordValid ;
+    
+        if(isLogginalid){
+ 
+              setTimeout(function(){
+                alert("Inicio Exitoso!");
+               bluredLogginSection.classList.toggle("bluredLoggin"); 
+               logginUsername.value = "";
+               logginPassword.value = "";
+            }, 1000);
+            
+              
+        }})
   };
   
-  init();
+  init()
